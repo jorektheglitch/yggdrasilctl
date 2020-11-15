43e1c19037916453297d2f07fde4500a42232b6b
jorektheglitch <lozcontrariuz@yandex.ru>
2020-11-15 06:51:13 +0300

    Added new exception type and documented exceptions

--- yggdrasilctl/errors.py
+++ yggdrasilctl/errors.py
@@ -1,1 +1,5 @@
-class APIError(Exception): pass
+class APIError(Exception):
+    "Base class for Yggdrasil admin API related errors."
+
+class APIUnreachable(APIError):
+    "Exception raised in case of unsucessful attemption to connect to API."
